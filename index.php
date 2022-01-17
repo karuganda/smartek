7c02873680ca0712c3cf5b3fa82423bddfea11cb
Karuganda Frank <41649691+karuganda@users.noreply.github.com>
2022-01-17 19:11:39 +0200

    Re-started UI

---
+++ index.php
@@ -0,0 +1,172 @@
+
+   <!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+      <link rel="icon" type="image/png" href="assets/img/favicon.png">
+      <title>
+         SMARTEK
+      </title>
+      <!--     Fonts and icons     -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+      <!-- Nucleo Icons -->
+      <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+      <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+      <!-- Font Awesome Icons -->
+      <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+      <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+      <!-- CSS Files -->
+      <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+   </head>
+   <body class="g-sidenav-show  bg-gray-100 virtual-reality">
+      <div>
+         <!-- Navbar -->
+         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+            <div class="container-fluid py-1 px-3">
+               <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                     <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                     <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Home</li>
+                  </ol>
+                  <h6 class="font-weight-bolder mb-0">Home</h6>
+               </nav>
+               <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                  <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                  <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                        <li class="nav-item">
+                           <a href="http://localhost/test/aboutus.html" class="nav-link text-muted" target="_blank">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="http://localhost/test/blog.html" class="nav-link text-muted" target="_blank">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                           <a href="http://localhost/test/contact.html" class="nav-link pe-0 text-muted" target="_blank">Contact</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <ul class="navbar-nav  justify-content-end">
+                     <li class="nav-item d-flex align-items-center">
+                        <a href="http://localhost/test/users/login.php" class="nav-link text-body font-weight-bold px-0">
+                        <i class="fa fa-user me-sm-1"></i>
+                        <span class="d-sm-inline d-none">Sign In</span>
+                        </a>
+                     </li>
+                    
+                     
+                  </ul>
+               </div>
+            </div>
+         </nav>
+         <!-- End Navbar -->
+      </div>
+      <div class="border-radius-xl mt-3 mx-3 position-relative" style="background-image: url('assets/img/background.png') ; background-size: cover;">
+         
+         <main class="main-content mt-1 border-radius-lg">
+            <div class="section min-vh-85 position-relative transform-scale-0 transform-scale-md-7">
+               <div class="container">
+                  <div class="row pt-10">
+                     <div class="col-lg-1 col-md-1 pt-5 pt-lg-0 ms-lg-5 text-center">
+                        
+                        <button class="btn btn-white border-radius-lg p-2 mt-2" type="button" data-bs-toggle="tooltip" data-bs-placement="left" title="Home">
+                        <i class="fas fa-home p-2"></i>
+                        </button>
+                        
+                     </div>
+                     <div class="col-lg-8 col-md-11">
+                        <div class="d-flex">
+                           <div class="me-auto">
+                           <h6 class="text-uppercase mb-0 ms-1">powered by</h6>
+                              <h1 class="display-1 font-weight-bold mt-n4 mb-0">  <a href="http://localhost/test/about.html">SMARTEK</a> </h1>
+                              
+                           </div>
+                           <div class="ms-auto">
+                              <img class="w-50 float-end mt-lg-n4" src="assets/img/small-logos/icon-sun-cloud.png" alt="image sun">
+                           </div>
+                        </div>
+                        <div class="row mt-4">
+                           
+                           <div class="col-lg-8 ">
+                              <div class="card bg-gradient-dark move-on-hover">
+                                 <div class="card-body">
+                                    <div class="d-flex">
+                                       <h5 class="mb-0 text-white">To Verify if the device has been flagged as stolen please enter the serial number in the text box below.
+                                             </h5>
+                                       
+                                    </div>
+                                    <p class="text-white mb-0"></p>
+                                    <p class="mb-0 text-white"></p>
+                                 </div>
+                                 <a href="javascript:;" class="w-100 text-center py-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Show More">
+                                 <i class="fas fa-chevron-down text-white"></i>
+                                 </a>
+                              </div>
+                              <div class="card move-on-hover mt-4">
+                                 <div class="card-body">
+                                 <div class="input-group">
+                                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+                                    <input type="text" class="form-control" placeholder="Enter phone number">
+                                    <a class="btn btn-link text-primary text-gradient px-3 mb-0" href="javascript:;"><i class="far fa-paper-plane me-2"></i>Verify</a>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </main>
+      </div>
+      <footer class="footer pt-3  ">
+         <div class="container-fluid">
+            <div class="row align-items-center justify-content-lg-between">
+               <div class="col-lg-6 mb-lg-0 mb-4">
+                  <div class="copyright text-center text-sm text-muted text-lg-start">
+                     © <script>
+                        document.write(new Date().getFullYear())
+                     </script>,
+                     made with <i class="fa fa-heart"></i> by
+                     <a href="smartek.com" class="font-weight-bold" target="_blank">SMARTEK</a>
+                    
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                     <li class="nav-item">
+                        <a href="http://localhost/test/aboutus.html" class="nav-link text-muted" target="_blank">About Us</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="http://localhost/test/blog.html" class="nav-link text-muted" target="_blank">Blog</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="http://localhost/test/contact.html" class="nav-link pe-0 text-muted" target="_blank">Contact</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </footer>
+      
+      <!--   Core JS Files   -->
+      <script src="assets/js/core/popper.min.js"></script>
+      <script src="assets/js/core/bootstrap.min.js"></script>
+      <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+      <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+      <script>
+         var win = navigator.platform.indexOf('Win') > -1;
+         if (win && document.querySelector('#sidenav-scrollbar')) {
+           var options = {
+             damping: '0.5'
+           }
+           Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+         }
+      </script>
+      <!-- Github buttons -->
+      <script async defer src="https://buttons.github.io/buttons.js"></script>
+      <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+      <script src="assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+   </body>
+</html>
